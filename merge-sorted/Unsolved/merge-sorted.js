1b40b52c931c149daa26f2b058c3eed700dacd61
Hunter McGrew <hunter109@gmail.com>
2022-07-14 14:46:43 -0500

    fixed one, i did not follow the readme

--- merge-sorted/Unsolved/merge-sorted.js
+++ merge-sorted/Unsolved/merge-sorted.js
@@ -6,27 +6,44 @@ var mergeSorted = function (arr1, arr2) {
 
     console.log("arr2", arr2);
 
-    // let newArr = [];
+    // go through each array and compare each number and push in sorted order
+    // new array to return 
+    let result = [];
 
-    // arr1.forEach(n => {
-    //    newArr.push(n);
-    // });
+    let new1 = 0;
+    let new2 = 0;
 
-    // arr2.forEach(n => {
-    //     newArr.push(n);
-    // });
+    // checks the num values against each other on every push request until every 
+    // item in the arrays are pushed.
+    // this works because the arrays are ALREADY sorted.
+    while (new1 < arr1.length && new2 < arr2.length) {
+        let num1 = arr1[new1];
+        let num2 = arr2[new2];
 
-    // console.log("newArr", newArr);
+        if (num1 < num2) {
+            result.push(num1);
+            new1++;
+        } else {
+            result.push(num2);
+            new2++;
+        };
+    };
 
-    // return newArr.sort((a, b) => a - b);
+    // starts at index 0, num = arr1[index], tries to push to result.
+    // on successful push new1 + 1
+    while (new1 < arr1.length) {
+        let num1 = arr1[new1];
+        result.push(num1);
+        new1++;
+    };
 
+    while (new2 < arr2.length) {
+        let num2 = arr2[new2];
+        result.push(num2);
+        new2++;
+    };                                      
+                        
+    console.log("result", result);
 
-    arr2.forEach(n => {
-        arr1.push(n);
-    })
-
-    console.log("arr1", arr1);
-
-    return arr1.sort((a, b) => a - b);
-
+    return result; 
 };
